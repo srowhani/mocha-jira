b7ece845cac1372d58cedbd58e4b9d639b47e549
Seena <seenarowhani95@gmail.com>
2016-07-13 13:28:07 -0400

    bulk of scaffolding

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
-
 /**
  * @author Seena Rowhani
  * @description Auto-generate test scaffolding using JIRA
  */
 ;(function (program, Jira, chalk, package) {
-  ;(function init () {
-    if (process.argv.length < 3)
-      throw Error('No arguments were supplied. Try `-h` for help.')
+  /**
+   * [parse description]
+   * @param  {[type]} comment [description]
+   * @return {[type]}         [description]
+   */
+  let parse = function (comment) {
+    let body = comment.body
+    
+  }
+  ;(init => {
     program
       .version(package.version)
       .option('-i, --issue <value>', 'Issue Key (REQUIRED)')
@@ -15,20 +21,24 @@
       .option('-u, --username <value>', 'Jira Username (REQUIRED)')
       .option('-p, --password <value>', 'Jira Password (REQUIRED)')
       .parse(process.argv)
-    if (!program.issue) {
-      throw Error('Issue Key (-i) must be specified')
-    }
   })();
 
-  var jira = new Jira({
+  let jira = new Jira({
     protocol: 'https',
-    host: 'jira.somehost.com',
-    username: 'username',
-    password: 'password',
+    host: program.host || process.env.JIRA_HOST,
+    username: program.username || process.env.JIRA_USER,
+    password: program.password || process.env.JIRA_PASS,
     apiVersion: '2',
     strictSSL: true
   });
-
+  jira.findIssue('BPSO-25131')
+    .then(issue => {
+      let comments = issue.fields.comment.comments
+      comments.forEach(parse)
+    }).catch(e => {
+      console.error(chalk.bold.red(e))
+      process.exit()
+    })
 
 })(
   require('commander'),
